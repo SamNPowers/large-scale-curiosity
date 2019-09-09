44dc0ce09621fcb6d599b3767219134b0a6117fe
Sam Powers <Sam.N.Powers@gmail.com>
2019-09-09 16:01:45 -0400

    Tensorflow mismatch. Also preparing for when the cluster actually supports later tfs.

--- run_wrappers/internal_yoda_runner.sh
+++ run_wrappers/internal_yoda_runner.sh
@@ -3,14 +3,17 @@
 CUDA_VISIBLE="$1"
 CONFIG_NAME="$2"
 
-CONDA_ENV="venv_curiosity"
+#CONDA_ENV="venv_curiosity"
+CONDA_ENV="venv_curiosity_tf1_12"
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 eval "$(conda shell.bash hook)"
 conda activate $CONDA_ENV
 
-module load cuda-10.0
-module load cudnn-10.0-7.3
+#module load cuda-10.0
+#module load cudnn-10.0-7.3
+module load cuda-9.1
+module load cudnn-9.1-7.1
 
 export PYTHONPATH="$CURRENT_DIR/../..":"$CURRENT_DIR/../../baselines"
 export CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE
